65c1cf9e4e9a2c3b3a67f1399a8119d96ff04ca5
kavimugilan.s@agilepoint.com <kavimugilan.s@agilepoint.com>
2021-04-19 10:06:22 +0530

    Updated photo gallery new images

--- src/components/Photo-Gallery/Photo-Gallery.js
+++ src/components/Photo-Gallery/Photo-Gallery.js
@@ -1,10 +1,127 @@
 import React from "react";
-import "../common/style.css";
+import "./gallery-style.css";
+import gallery1 from "../../image/gallery/gallery-1.JPG";
+import gallery2 from "../../image/gallery/gallery-2.JPG";
+import gallery3 from "../../image/gallery/gallery-3.JPG";
+import gallery4 from "../../image/gallery/gallery-4.jpg";
+import gallery5 from "../../image/gallery/gallery-5.jpg";
+import gallery6 from "../../image/gallery/gallery-6.jpg";
+import gallery7 from "../../image/gallery/gallery-7.jpg";
+import gallery8 from "../../image/gallery/gallery-8.JPG";
+import gallery9 from "../../image/gallery/gallery-9.JPG";
+import gallery10 from "../../image/gallery/gallery-10.JPG";
+import gallery11 from "../../image/gallery/gallery-11.JPG";
+import gallery12 from "../../image/gallery/gallery-12.JPG";
+import gallery13 from "../../image/gallery/gallery-13.JPG";
+import gallery14 from "../../image/gallery/gallery-14.JPG";
+import gallery15 from "../../image/gallery/gallery-15.JPG";
+import gallery16 from "../../image/gallery/gallery-16.JPG";
+import gallery17 from "../../image/gallery/gallery-17.JPG";
+import gallery18 from "../../image/gallery/gallery-18.JPG";
+import gallery19 from "../../image/gallery/gallery-19.JPG";
+import gallery20 from "../../image/gallery/gallery-20.JPG";
+import gallery21 from "../../image/gallery/gallery-21.JPG";
+import gallery22 from "../../image/gallery/gallery-22.JPG";
+import gallery23 from "../../image/gallery/gallery-23.jpg";
+import gallery24 from "../../image/gallery/gallery-24.jpg";
+import gallery25 from "../../image/gallery/gallery-25.jpg";
+import gallery26 from "../../image/gallery/gallery-26.jpg";
+import gallery27 from "../../image/gallery/gallery-27.jpg";
+import gallery28 from "../../image/gallery/gallery-28.jpg";
+import gallery29 from "../../image/gallery/gallery-29.jpg";
+import gallery30 from "../../image/gallery/gallery-30.jpg";
+import gallery31 from "../../image/gallery/gallery-31.jpeg";
+import gallery32 from "../../image/gallery/gallery-32.jpeg";
 
 const PhotoGallery = () => {
     return (
-        <div class="comingSoon">
-           PhotoGallery - Coming Soon...
+        <div class="grid-container">
+          <div>
+            <img class='grid-item grid-item-1' src={gallery1} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-2' src={gallery2} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-3' src={gallery3} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-4' src={gallery4} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-5' src={gallery5} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-6' src={gallery6} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-7' src={gallery7} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-8' src={gallery8} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-9' src={gallery9} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-10' src={gallery10} alt=''/>
+          </div>
+
+          <div>
+            <img class='grid-item grid-item-1' src={gallery11} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-2' src={gallery12} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-3' src={gallery13} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-4' src={gallery14} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-5' src={gallery15} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-6' src={gallery16} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-7' src={gallery17} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-8' src={gallery18} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-9' src={gallery19} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-10' src={gallery20} alt=''/>
+          </div>
+          <div>
+            <img class='grid-item grid-item-10' src={gallery21} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery22} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery23} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery24} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery25} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery26} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery27} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery28} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery29} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery30} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery31} alt=''/>
+          </div> <div>
+            <img class='grid-item grid-item-10' src={gallery32} alt=''/>
+          </div>
         </div>
     );
 };
