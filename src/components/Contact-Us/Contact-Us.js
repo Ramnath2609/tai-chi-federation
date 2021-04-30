c6b4d98eb40c20c123fd3f1cf9483993e515d36d
Ramnath K <ramnath2609@gmail.com>
2021-04-30 09:56:12 +0530

    contact us details added

--- src/components/Contact-Us/Contact-Us.js
+++ src/components/Contact-Us/Contact-Us.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "../common/style.css";
+import { Container } from "reactstrap"
 
 const ContactUS = () => {
     return (
-        <div class="comingSoon">
-           Contact - Coming Soon...
-        </div>
+        <Container>
+            <div className="contact-us">
+                <p>Anybody who wants to learn Taichi or wants to be a member of the Taichi Federation of India shall
+                contact the Managing Trustee of Taichi Federation of India, Mrs. Rajam Selvaraj in the following</p>
+                <p>Address:</p>
+                <p>Mrs. Rajam Selvaraj</p>
+                <p>Managing Trustee</p>
+                <p>Taichi Federation of India</p>
+                <p>15- B 6, Nesamony Nager</p>
+                <p>Nagercoil – 629002</p>
+                <p>Mobile: 98430 91146</p>
+                <p>E mail:</p>
+            </div>
+        </Container>
     );
 };
 
