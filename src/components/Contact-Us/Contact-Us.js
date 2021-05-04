3463404f573da669b95ed05bd16cc0e5ca20b1f3
Ramnath K <ramnath2609@gmail.com>
2021-05-04 15:09:45 +0530

    contact us

--- src/components/Contact-Us/Contact-Us.js
+++ src/components/Contact-Us/Contact-Us.js
@@ -6,16 +6,18 @@ const ContactUS = () => {
     return (
         <Container>
             <div className="contact-us">
-                <p>Anybody who wants to learn Taichi or wants to be a member of the Taichi Federation of India shall
-                contact the Managing Trustee of Taichi Federation of India, Mrs. Rajam Selvaraj in the following</p>
-                <p>Address:</p>
-                <p>Mrs. Rajam Selvaraj</p>
-                <p>Managing Trustee</p>
-                <p>Taichi Federation of India</p>
-                <p>15- B 6, Nesamony Nager</p>
-                <p>Nagercoil – 629002</p>
-                <p>Mobile: 98430 91146</p>
-                <p>E mail:</p>
+                <h3>Anybody who wants to learn Taichi or wants to be a member of the Taichi Federation of India shall
+                contact the Managing Trustee of Taichi Federation of India, Mrs. Rajam Selvaraj in the following :</h3>
+                <div className="contact-us-card">
+                    <p>Address:</p>
+                    <p>Mrs. Rajam Selvaraj</p>
+                    <p>Managing Trustee</p>
+                    <p>Taichi Federation of India</p>
+                    <p>15- B 6, Nesamony Nager</p>
+                    <p>Nagercoil – 629002</p>
+                    <p>Mobile: 98430 91146</p>
+                    <p>E mail:</p>
+                </div>
             </div>
         </Container>
     );
